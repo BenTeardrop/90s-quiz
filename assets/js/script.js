605f3565318067eca2c244c8868ddfab1ee88fc5
Ben Jim <jimenezbenjamin@yahoo.com>
2022-03-08 14:53:48 +0000

    add array of 5 question to the js page

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let questionList = [{
+    question: 'what car was the time machine in Back to the future?',
+    answers: ['Porshe', 'Ford', 'Deloreane'],
+    correctAnswer: 'deloreane'
+}, {
+    question: 'what model T was the bady in terminator 2?',
+    answers: ['T total', 'T 1000', 'T - 2000'],
+    correctAnswer: 'T - 200'
+}, {
+    question: 'What year was forest gump released?',
+    answers: ['1991', '1994', '1996'],
+    correctAnswer: '1994',
+}, {
+    question: 'Which artist sang the song “Oh, Pretty Woman” from the film Pretty Woman?',
+    answers: ['Roy Orbison', 'Johnny Cash', 'clint Eastwood'],
+    correctAnswer: 'Roy Orbison',
+}, {
+    question: 'Which artist sang the hit “Unchained Melody” from the film Ghost?',
+    answers: ['The Righteous Brothers', 'The Isley brothers', 'The Blues Brothers'],
+    correctAnswer: 'The Righteous Brothers',
+}]
